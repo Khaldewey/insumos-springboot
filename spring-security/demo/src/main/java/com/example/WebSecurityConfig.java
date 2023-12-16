056ba0eae3b3ae224db38b12c254906b969753b8
Israel <israel_alves77@hotmail.com>
2023-12-15 22:06:26 -0300

    correção do configsecurity e implementação moderna do adapter

--- spring-security/demo/src/main/java/com/example/WebSecurityConfig.java
+++ spring-security/demo/src/main/java/com/example/WebSecurityConfig.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
   @Bean
@@ -23,6 +28,18 @@ public class WebSecurityConfig {
         .roles("USER", "ADMIN")
         .build();
     return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests((requests) -> requests
+        .requestMatchers("/", "/login").permitAll()
+        .requestMatchers("/admin").hasAnyRole("ADMIN")
+        .requestMatchers("/users").hasAnyRole("USER", "ADMIN")
+        .anyRequest()
+        .authenticated()).formLogin(withDefaults())
+        .build();
+
   }
 
 }
