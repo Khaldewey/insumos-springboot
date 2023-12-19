5825d78840bb392c15dabb7a051d33d3d182fb68
Israel <israel_alves77@hotmail.com>
2023-12-18 21:54:11 -0300

    jwt

--- spring-security/demo/src/main/java/com/example/WebSecurityConfig.java
+++ spring-security/demo/src/main/java/com/example/WebSecurityConfig.java
@@ -8,52 +8,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 // import org.springframework.security.core.userdetails.User;
 // import org.springframework.security.core.userdetails.UserDetails;
 // import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 // import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
-
-
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 public class WebSecurityConfig {
-    @Autowired
-    private SecurityDatabaseService securityService;
+  @Autowired
+  private SecurityDatabaseService securityService;
 
-    
-
-    
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(securityService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-  // @Bean
-  // public UserDetailsService users() {
-  //   UserDetails user = User.builder()
-  //       .username("user")
-  //       .password("{noop}user123")
-  //       .roles("USER")
-  //       .build();
-  //   UserDetails admin = User.builder()
-  //       .username("admin")
-  //       .password("{noop}admin123")
-  //       .roles("USER", "ADMIN")
-  //       .build();
-  //   return new InMemoryUserDetailsManager(user, admin);
-  // }
+  @Autowired
+  public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+  }
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    
+
     return http.authorizeHttpRequests((requests) -> requests
         .requestMatchers("/", "/login").permitAll()
-        .requestMatchers("/admin").hasAnyRole("ADMIN")
-        .requestMatchers("/users").hasAnyRole("USER", "ADMIN")
+        .requestMatchers("/admin").hasAnyRole("MANAGERS")
+        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
         .anyRequest()
         .authenticated()).httpBasic(withDefaults())
         .build();
